245752c5694fd78a7a381397ac8583b29b5957c9
Matthew Adams <matthew.adams@endjin.com>
2024-02-28 14:44:28 +0000

    Initial suggested implementation for optional, nullable, and optional-nullable wrapper types.

---
+++ Solutions/Corvus.Json.ExtendedTypes/Corvus.Json/JsonNullable{T}.cs
@@ -0,0 +1,93 @@
+﻿// <copyright file="JsonNullable{T}.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+using System.Text.Json;
+
+namespace Corvus.Json;
+
+/// <summary>
+/// Represents a JSON property value that might be nullable.
+/// </summary>
+/// <typeparam name="T">The type of the value.</typeparam>
+public readonly struct JsonNullable<T>
+    where T : struct, IJsonValue<T>
+{
+    private readonly T backing;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="JsonNullable{T}"/> class.
+    /// </summary>
+    /// <param name="value">The potentially nullable value.</param>
+    public JsonNullable(in T value)
+    {
+        this.backing = value;
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether this nullable value is not null.
+    /// </summary>
+    /// <remarks>
+    /// Note that this determines that the value is not <see cref="JsonValueKind.Null"/>. If the value
+    /// may be <see cref="JsonValueKind.Null"/> then this will still return <see langword="true"/>.
+    /// </remarks>
+    public bool HasValue => this.backing.ValueKind != JsonValueKind.Null;
+
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown if the nullable value is undefined.</exception>
+    public T Value
+    {
+        get
+        {
+            if (!this.HasValue)
+            {
+                throw new InvalidOperationException("The nullable value is undefined.");
+            }
+
+            return this.backing;
+        }
+    }
+
+    /// <summary>
+    /// Explicit conversion to the value.
+    /// </summary>
+    /// <param name="value">The nullable backing value to convert.</param>
+    public static explicit operator T(JsonNullable<T> value) => value.backing;
+
+    /// <summary>
+    /// Implicit conversion to an nullable value.
+    /// </summary>
+    /// <param name="value">The value to convert.</param>
+    public static implicit operator JsonNullable<T>(T value) => new(value);
+
+    /// <summary>
+    /// Gets the value of the current instance, or a default value.
+    /// </summary>
+    /// <param name="defaultValue">The default value to use if <see cref="HasValue"/> is <see langword="false"/>.</param>
+    /// <returns>The value of the <see cref="Value"/> property if the <see cref="HasValue"/> property is <see langword="true"/>; otherwise, the supplied <paramref name="defaultValue"/>.</returns>
+    public readonly T GetValueOrDefault(in T defaultValue) => this.HasValue ? this.backing : defaultValue;
+
+    /// <inheritdoc/>
+    public override bool Equals(object? other)
+    {
+        if (!this.HasValue)
+        {
+            return other == null;
+        }
+
+        if (other is T value)
+        {
+            return this.backing.Equals(value);
+        }
+
+        return false;
+    }
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => this.HasValue ? this.backing.GetHashCode() : 0;
+
+    /// <inheritdoc/>
+    public override string? ToString() => this.HasValue ? this.backing.ToString() : string.Empty;
+}
